a182d6282ea9b091023051bfa29c992e52d93384
Elias Pintos <elias.pintos@tecnilogica.com>
2017-08-09 13:28:06 +0200

    css arquitecture

--- src/ts/store.ts
+++ src/ts/store.ts
@@ -4,3 +4,15 @@ import reducer from './reducers/'
 const store = createStore(reducer);
 
 export default store;
+
+
+
+// // or you can just import "subscribe" function from the package
+// import {subscribe} from 'redux-subscriber';
+
+// const unsubscribe = subscribe('user.messages.count', state => {
+//     // do something
+// });
+
+// // if you want to stop listening to changes
+// unsubscribe();
