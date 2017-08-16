1a65ba72d6ac6fc678c0d76bbf597e6d3a020f14
Elias Pintos <elias.pintos@tecnilogica.com>
2017-08-16 21:04:54 +0200

    Tweeks on prebuild script

--- builder_scripts/prebuild.js
+++ builder_scripts/prebuild.js
@@ -8,23 +8,29 @@ function builder(){
 
 	const pages = fs.readdirSync('./src/pages', {encoding: 'utf8'});
 	const includes = pages.map( (file) => `<%- include('pages/${file}'); %>`).join('');
+	const pageNames = pages.map( (file) => 
+		file
+			.replace(/\./g, '')
+			.replace('ejs', '')
+			.replace(/[0-9]/g, '')
+	);	
+
 	const index = 
 		fs
 			.readFileSync('./src/index.html', {encoding: 'utf8'})
 			.toString()
 			.replace('###', includes);
-			//.map( (file) => 
-			//file
-				//.replace(/\./g, '')
-				//.replace('ejs', '')
-				//.replace(/[0-9]/g, '')
-		//)	
+			
 	fs
 		.writeFileSync(
 			'./dist/index.html', 
 			ejs
 				.render(
-					index, {filename: './src/index.html'}
+					index, 
+					{
+						filename: './src/index.html',
+						pages: pageNames
+					}
 				)
 		);
 
--- src/ts/components/carousel/carousel.component.ts
+++ src/ts/components/carousel/carousel.component.ts
@@ -1,1 +1,12 @@
-console.log('aaa');
+import {Component, ComponentClass} from "../component.ts";
+
+export default class NavBar extends ComponentClass implements Component {
+	
+	renderable: boolean = false;
+	storeProperty: string = "";
+	selector: string;
+
+	constructor(selector: string = "carousel") {
+		super(selector);
+	}
+}
